0c1c234313b32247fc61c73e67c746f5d476ff5e
David Hale <david.hale7@gmail.com>
2018-01-22 11:09:32 -0800

    basic files

---
+++ byteshopFront/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Byteshop!!!</Text>
+        <Text>This is our front end app!</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
